69f778cdc20b105afc7a5efc465f9b998b9cc649
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-02-02 08:39:25 +0300

    add swagger

---
+++ src/Analytics.Api/AppStart/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace Analytics.Api.AppStart
+{
+    public class Startup
+    {
+        private WebApplicationBuilder _builder;
+
+        public Startup(WebApplicationBuilder builder)
+        {
+            _builder = builder ?? throw new ArgumentNullException(nameof(builder));
+        }
+
+        public void Initialize()
+        {
+            if (_builder.Environment.IsDevelopment())
+            {
+                _builder.Services.AddSwaggerGen();
+            }
+
+            //InitConfigs();
+            
+            _builder.Services.AddControllers();
+        }
+    }
+}
---
+++ src/Analytics.Api/Program.cs
@@ -0,0 +1,26 @@
+using Analytics.Api.AppStart;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder);
+startup.Initialize();
+
+var app = builder.Build();
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+app.Logger.LogInformation(environment ?? "Empty environment");
+// Configure the HTTP request pipeline.
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
