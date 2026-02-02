85c9a1fbd489ffeeb7c607e99a5e2052efbb8765
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-02-02 08:44:49 +0300

    add AnalyticsController

---
+++ src/Analytics.Api/Controllers/AnalyticsController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Analytics.Api.Controllers
+{
+    [Route("v1/analytics")]
+    [ApiController]
+    public class AnalyticsController : ControllerBase
+    {
+        private readonly ILogger<AnalyticsController> _logger;
+        public AnalyticsController(ILogger<AnalyticsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Test()
+        {
+            _logger.LogInformation("call test");
+            return "123";
+        }
+    }
+}
