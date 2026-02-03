6445e32c2cfae1de133d66f9c019af76cd65619c
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-02-03 05:59:16 +0300

    add tests

--- src/Analytics.Api/BLL/Services/UserAgentParser.cs
+++ src/Analytics.Api/BLL/Services/UserAgentParser.cs
@@ -24,5 +24,57 @@
 
             return (os, browser, device);
         }
+
+        public static (string OS, string Browser, string DeviceType) Parse2(string userAgent)
+        {
+            if (string.IsNullOrEmpty(userAgent))
+                return ("Unknown", "Unknown", "Desktop");
+
+            var ua = userAgent.ToLowerInvariant();
+
+            // Определение ОС (проверяем в правильном порядке)
+            string os = "Unknown";
+
+            if (ua.Contains("android"))
+                os = "Android";
+            else if (ua.Contains("iphone") || ua.Contains("ipad") || ua.Contains("ipod"))
+                os = "iOS";
+            else if (ua.Contains("mac os x"))
+                os = "macOS";
+            else if (ua.Contains("windows"))
+                os = "Windows";
+            else if (ua.Contains("linux"))
+                os = "Linux";
+
+            // Определение браузера (более точная проверка)
+            string browser = "Unknown";
+
+            if (ua.Contains("edg/") || ua.Contains("edge/"))
+                browser = "Edge";
+            else if (ua.Contains("opr/") || ua.Contains("opera"))
+                browser = "Opera";
+            else if (ua.Contains("chrome") && !ua.Contains("chromium"))
+                browser = "Chrome";
+            else if (ua.Contains("firefox") || ua.Contains("fxios"))
+                browser = "Firefox";
+            else if (ua.Contains("safari") && !ua.Contains("chrome"))
+                browser = "Safari";
+            else if (ua.Contains("trident") || ua.Contains("msie"))
+                browser = "Internet Explorer";
+
+            // Определение типа устройства
+            string device = "Desktop";
+
+            if (ua.Contains("mobile") || os == "Android" || os == "iOS")
+                device = "Mobile";
+            if (ua.Contains("tablet") || (os == "iOS" && ua.Contains("ipad")))
+                device = "Tablet";
+            if (ua.Contains("smart-tv") || ua.Contains("smarttv"))
+                device = "TV";
+            if (ua.Contains("bot") || ua.Contains("crawler") || ua.Contains("spider"))
+                device = "Bot";
+
+            return (os, browser, device);
+        }
     }
 }
---
+++ tests/Analytics.Api.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Analytics.Api.BLL.Services;
+
+namespace Analytics.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var userAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36\r\n";
+            var res = UserAgentParser.Parse(userAgent);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var userAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36\r\n";
+            var res = UserAgentParser.Parse2(userAgent);
+        }
+    }
+}
